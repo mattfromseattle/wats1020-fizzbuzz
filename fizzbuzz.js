0489dea436d29202890e7d691ecb125fc044eb6b
Matt Dewey <mattfromseattle@me.com>
2017-01-02 10:42:46 -0800

    Fixing variable names.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,13 @@
 // Code for the FizzBuzz project for WATS3020
 
 // Count through numbers 1 to 20 and determine if the number is divisible by 3, 5, or both.
-for (FizzBuzzNumber = 1; FizzBuzzNumber <= 20; FizzBuzzNumber++) {
+for (fizzBuzzNumber = 1; fizzBuzzNumber <= 20; fizzBuzzNumber++) {
     // For each number, do the following: 
 
     // Is number divisible by 3?
-    if (FizzBuzzNumber % 3 === 0) {
+    if (fizzBuzzNumber % 3 === 0) {
         // if yes, check if number is also divisible by 5
-        if (FizzBuzzNumber % 5 === 0) {
+        if (fizzBuzzNumber % 5 === 0) {
             // If divisible by 3 AND 5: Print FizzBuzz
             console.log('FizzBuzz');
         } else {
@@ -18,11 +18,11 @@ for (FizzBuzzNumber = 1; FizzBuzzNumber <= 20; FizzBuzzNumber++) {
     }
     // Is number divisible by 5?
     // If yes: Print Buzz
-    else if (FizzBuzzNumber % 5 === 0) {
+    else if (fizzBuzzNumber % 5 === 0) {
         console.log('Buzz');
                 // Is number NOT divisible by 3 or 5?
-        // If yes, Print number
+        // If yes, Print Number
     } else {
-        console.log(FizzBuzzNumber);
+        console.log(fizzBuzzNumber);
     }
 }
